ca70f128eacbe77fea74777483a9d307e4b895a1
croissant.Hi <croissant.hi@outlook.jp>
2016-06-08 00:17:15 +0900

    リモートにアップ

---
+++ Gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt){
+    grunt.initConfig({
+        pkg:grunt.file.readJSON("package.json"),
+        slug: 'kawahagi',
+        dir: {
+            theme: 'kawahagi_love',
+            sass: '_sass',
+            css: '_css',
+
+        },
+
+        compass: {
+            dist: {
+                options: {
+                    config: 'config.rb'
+                }
+            }
+        },
+
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= dir.css %>',
+                    src: ['*.css', '!*.min.css'],
+                    dest: '<%= dir.theme %>/css',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'dev',
+                src: ['css/**', 'js/**', '**.html', '**.php', 'images/**'],
+                dest: 'release/'
+            }
+        },
+
+        connect: {
+            server: {
+                options: {
+                    //livereload: true,
+                    hostname: '*',
+                    port: 8001,
+                    base: 'dev'
+                }
+            }
+        },
+
+        watch: {
+            compass:{
+                tasks: ['compass'],
+                files:['<%= dir.sass %>/<%= slug %>.scss']
+            },
+            cssmin:{
+                tasks: ['cssmin'],
+                files:['<%= dir.css %>/<%= slug %>.css']
+            },
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('build', ['compass', 'cssmin', 'copy']);
+    grunt.registerTask('default', ['compass', 'cssmin']);
+}
