19fbf7157f8476037f83f71226ecb042b894dbbd
croissant.Hi <croissant.hi@outlook.jp>
2016-06-10 23:35:00 +0900

    トップページ

--- kawahagi_love/functions.php
+++ kawahagi_love/functions.php
@@ -7,6 +7,15 @@
  * @package Kawahagi_Wo_Mederukai
  */
 
+
+//画像のパス短縮用
+function imagepassshort($arg) {
+    $content = str_replace('"images/', '"' . get_bloginfo('template_directory') . '/images/', $arg);
+    return $content;
+}
+add_action('the_content', 'imagepassshort');
+
+
 if ( ! function_exists( 'kawahagi_love_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -16,66 +25,66 @@ if ( ! function_exists( 'kawahagi_love_setup' ) ) :
  * as indicating support for post thumbnails.
  */
 function kawahagi_love_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Kawahagi Wo Mederukai, use a find and replace
-	 * to change 'kawahagi_love' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'kawahagi_love', get_template_directory() . '/languages' );
+    /*
+     * Make theme available for translation.
+     * Translations can be filed in the /languages/ directory.
+     * If you're building a theme based on Kawahagi Wo Mederukai, use a find and replace
+     * to change 'kawahagi_love' to the name of your theme in all the template files.
+     */
+    load_theme_textdomain( 'kawahagi_love', get_template_directory() . '/languages' );
 
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+    // Add default posts and comments RSS feed links to head.
+    add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
+    /*
+     * Let WordPress manage the document title.
+     * By adding theme support, we declare that this theme does not use a
+     * hard-coded <title> tag in the document head, and expect WordPress to
+     * provide it for us.
+     */
+    add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-	 */
-	add_theme_support( 'post-thumbnails' );
+    /*
+     * Enable support for Post Thumbnails on posts and pages.
+     *
+     * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+     */
+    add_theme_support( 'post-thumbnails' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'kawahagi_love' ),
-	) );
+    // This theme uses wp_nav_menu() in one location.
+    register_nav_menus( array(
+        'primary' => esc_html__( 'Primary', 'kawahagi_love' ),
+    ) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+    /*
+     * Switch default core markup for search form, comment form, and comments
+     * to output valid HTML5.
+     */
+    add_theme_support( 'html5', array(
+        'search-form',
+        'comment-form',
+        'comment-list',
+        'gallery',
+        'caption',
+    ) );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
+    /*
+     * Enable support for Post Formats.
+     * See https://developer.wordpress.org/themes/functionality/post-formats/
+     */
+    add_theme_support( 'post-formats', array(
+        'aside',
+        'image',
+        'video',
+        'quote',
+        'link',
+    ) );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'kawahagi_love_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+    // Set up the WordPress core custom background feature.
+    add_theme_support( 'custom-background', apply_filters( 'kawahagi_love_custom_background_args', array(
+        'default-color' => 'ffffff',
+        'default-image' => '',
+    ) ) );
 }
 endif;
 add_action( 'after_setup_theme', 'kawahagi_love_setup' );
@@ -88,7 +97,7 @@ add_action( 'after_setup_theme', 'kawahagi_love_setup' );
  * @global int $content_width
  */
 function kawahagi_love_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'kawahagi_love_content_width', 640 );
+    $GLOBALS['content_width'] = apply_filters( 'kawahagi_love_content_width', 640 );
 }
 add_action( 'after_setup_theme', 'kawahagi_love_content_width', 0 );
 
@@ -98,15 +107,15 @@ add_action( 'after_setup_theme', 'kawahagi_love_content_width', 0 );
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function kawahagi_love_widgets_init() {
-	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'kawahagi_love' ),
-		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'kawahagi_love' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+    register_sidebar( array(
+        'name'          => esc_html__( 'Sidebar', 'kawahagi_love' ),
+        'id'            => 'sidebar-1',
+        'description'   => esc_html__( 'Add widgets here.', 'kawahagi_love' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 add_action( 'widgets_init', 'kawahagi_love_widgets_init' );
 
@@ -114,15 +123,15 @@ add_action( 'widgets_init', 'kawahagi_love_widgets_init' );
  * Enqueue scripts and styles.
  */
 function kawahagi_love_scripts() {
-	wp_enqueue_style( 'kawahagi_love-style', get_stylesheet_uri() );
+    wp_enqueue_style( 'kawahagi_love-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'kawahagi_love-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+    wp_enqueue_script( 'kawahagi_love-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'kawahagi_love-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+    wp_enqueue_script( 'kawahagi_love-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
+    if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+        wp_enqueue_script( 'comment-reply' );
+    }
 }
 add_action( 'wp_enqueue_scripts', 'kawahagi_love_scripts' );
 
@@ -150,3 +159,5 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+
--- kawahagi_love/index.php
+++ kawahagi_love/index.php
@@ -11,7 +11,7 @@
  *
  * @package Kawahagi_Wo_Mederukai
  */
-
+$theme_url = get_template_directory_uri();
 ?>
 <!DOCTYPE html>
 <html>
@@ -20,56 +20,93 @@
         <meta charset="UTF-8">
         <meta name="description" content="同人サークルカワハギを愛出る会オフィシャルサイトです。コミケなどのイベントを中心に、イラスト制作やマンガ制作などの同人活動を行っております。ジャンルは基本オリジナルです。" />
         <meta name="keywords" content="同人,同人サークル,カワハギを愛出る会" />
-        <link rel="stylesheet" type="text/css" href="style.css">
-        <link rel="stylesheet" type="text/css" href="css/kawahagi.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $theme_url; ?>/style.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo $theme_url; ?>/css/kawahagi.min.css">
     </head>
     <body>
-        <header>
-            <h1>同人サークル カワハギを愛出る会 オフィシャルサイト</h1>
-            <p id="copy"><small>Copyright &copy; カワハギを愛出る会 All Rights Reserved.</small></p>
-        </header>
+        <div id="wrapper">
+            <header>
+                <div id="header_wrap">
+                    <h1>同人サークル カワハギを愛出る会 オフィシャルサイト</h1>
+                    <p id="copy"><small>Copyright &copy; カワハギを愛出る会 All Rights Reserved.</small></p>
+                </div><!-- /header_wrap -->
+            </header>
 
-        <div id="top_main">
-            <div id="top_main_wrap"></div><!-- /top_main_wrap -->
-        </div><!-- /top_main -->
+            <div id="top_main">
+                <div id="top_main_wrap">
+                    <div id="top_main_box">
+                        <div id="top_main_title_box">
+                            <p id="top_main_logo"><a href="/"><img src="<?php echo $theme_url; ?>/images/main_logo.png" alt="カワハギを愛出る会"></a></p>
+                            <h2 id="top_main_title">カワハギを愛出る会 OfficialSite<br>kawahagi wo mederukai</h2>
+                            <div class="marquee">カワハギを愛出る会へようこそ！</div>
+                        </div><!-- /top_main_title_box -->
+                        <div class="main_img1"><img src="<?php echo $theme_url; ?>/images/main_img1.png" alt="イラスト1"></div>
+                        <div class="main_img2"><img src="<?php echo $theme_url; ?>/images/main_img2.png" alt="イラスト2"></div>
+                        <div class="main_img3"><img src="<?php echo $theme_url; ?>/images/main_img3.png" alt="イラスト3"></div>
+                    </div><!-- /top_main_box -->
+                </div><!-- /top_main_wrap -->
+            </div><!-- /top_main -->
 
-        <div id="top_nav">
-            <div id="top_nav_wrap">
-                <ul>
-                    <li></li>
-                </ul>
-                <div class="tv"></div><!-- /tv -->
-            </div><!-- /top_nav_wrap -->
-        </div><!-- /top_nav -->
+            <div id="top_nav">
+                <div id="top_nav_wrap">
+                    <div class="top_nav_box">
+                        <ul>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_home.png" alt="home"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_circle.png" alt="circle"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_gallery.png" alt="gallery"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_blog.png" alt="blog"></a></li>
+                        </ul>
+                    </div><!-- /top_nav_box -->
+                    <div class="tv">
+                        <img src="<?php echo $theme_url; ?>/images/nav_tv.png" alt="テレビ">
+                    </div><!-- /tv -->
+                </div><!-- /top_nav_wrap -->
+            </div><!-- /top_nav -->
 
-        <div id="contents">
-            <div id="contents_wrap">
-                <div id="main_contents">
-                <div class="banner_box">
+            <div id="contents">
+                <div id="contents_wrap" class="clear">
+                    <div id="main_contents">
+                        <div class="banner_box">
+                            <img src="<?php echo $theme_url; ?>/images/banner01.png" alt="">
+                        </div><!-- banner_box -->
 
-                    </div><!-- banner_box -->
-                    <div class="info_box">
+                        <h3 id="info_title"><img src="<?php echo $theme_url; ?>/images/title_info.png" alt="インフォメーション"></h3>
+                        <div class="info_box">
+                        <ul class="info_list">
+                            <li>
+                                <p class="info_date">2016.05.01</p>
+                                <p class="info_title">WEBサイトリニューアル！！</p>
+                            </li>
+                        </ul>
+                        </div><!-- /info_box -->
 
-                    </div><!-- /info_box -->
-                </div><!-- /main_contents -->
-                <div id="side_contents">
-                    <div class="sns_box">
+                    </div><!-- /main_contents -->
+                    <div id="side_contents">
+                        <div class="sns_box">
 
-                    </div><!-- /sns_box -->
-                </div><!-- /side_contents -->
-            </div><!-- /contents_wrap -->
-        </div><!-- /contents -->
+                        </div><!-- /sns_box -->
+                    </div><!-- /side_contents -->
+                </div><!-- /contents_wrap -->
+            </div><!-- /contents -->
 
-        <div id="foot_nav">
-            <div id="foot_nav_wrap">
-                <nav>
-                    <ul>
-                        <li></li>
-                    </ul>
-                </nav>
-                <div class="tv"></div><!-- /tv -->
-            </div><!-- /foot_nav_wrap -->
-        </div><!-- /foot_nav -->
-
+            <div id="foot_nav">
+                <div id="foot_nav_wrap">
+                    <nav>
+                        <ul>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_home.png" alt="home"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_circle.png" alt="circle"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_gallery.png" alt="gallery"></a></li>
+                            <li><a href=""><img src="<?php echo $theme_url; ?>/images/nav_blog.png" alt="blog"></a></li>
+                        </ul>
+                    </nav>
+                    <div class="tv">
+                        <img src="<?php echo $theme_url; ?>/images/nav_tv.png" alt="テレビ">
+                    </div><!-- /tv -->
+                    <div class="page_top">
+                        <a href=""><img src="<?php echo $theme_url; ?>/images/page_top.png" alt="ページTOPへ"></a>
+                    </div><!-- /page_top -->
+                </div><!-- /foot_nav_wrap -->
+            </div><!-- /foot_nav -->
+        </div><!-- /wrapper -->
     </body>
 </html>
